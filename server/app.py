c8647b5e89d08bee852a3e84ee40a17888a1a078
benedict langat <benedictlangat2000@gmail.com>
2023-10-20 15:30:57 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -6,3 +6,39 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+  
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)  # This will print the string to the console
+    return f"<p>Printed: {text}</p>"
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers = '\n'.join(map(str, range(1, num + 1)))
+    return f"<pre>{numbers}</pre>"
+
+@app.route('/math/<int:num1><string:operation><int:num2>')
+def math(num1, operation, num2):
+    result = 0
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division by zero is not allowed."
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return "Modulus by zero is not allowed."
+
+    return f"<p>{num1} {operation} {num2} = {result}</p>"
